6e721fc5187a6cb5f501f36b8d8b1dd9cba738fc
Kingston Lee <kingstonlee@gmail.com>
2016-09-30 10:09:02 -0700

    trying something

--- packages.sh
+++ packages.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 # Does Apache2 exist?
+installlist="apache2 php5"
+
+for package in installlist;
+do
+  if dpkg -s ${package} > /dev/null 2>&1 ; then
+    echo "${package} already installed"
+  else
+    sudo apt-get -y install ${package}
+  fi
+done
+
 if dpkg -s "apache2" > /dev/null 2>&1 ; then
  echo "apache2 already installed"
 else
@@ -14,3 +25,9 @@ else
  # Install PHP
  sudo apt-get -y install php5
 fi
+
+for
+
+function install() {
+  sudo apt-get -y install $service
+}
