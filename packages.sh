cee4c68f67d5d38eaa898a157575ba0b90b6da9f
Kingston Lee <kingstonlee@gmail.com>
2016-09-30 10:19:24 -0700

    function

--- packages.sh
+++ packages.sh
@@ -2,30 +2,19 @@
 # Does Apache2 exist?
 installlist="apache2 php5"
 
+function packageInstall() {
+  sudo apt-get -y install $service
+}
+
+function packageUninstall() {
+  sudo apt-get -y remove $service
+}
+
 for package in ${installlist};
 do
   if dpkg -s ${package} > /dev/null 2>&1 ; then
     echo "${package} already installed"
   else
-    sudo apt-get -y install ${package}
+    packageInstall ${package}
   fi
 done
-
-if dpkg -s "apache2" > /dev/null 2>&1 ; then
- echo "apache2 already installed"
-else
- # Install Apache2
- sudo apt-get -y install apache2
-fi
-
-# Does php exist?
-if dpkg -s "php5" > /dev/null 2>&1 ; then
- echo "php5 already installed"
-else
- # Install PHP
- sudo apt-get -y install php5
-fi
-
-function install() {
-  sudo apt-get -y install $service
-}
